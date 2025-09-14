d066b6b3d77bac66d9bbe9126bbec6343a3c1483
thazaww <thazaww@gmail.com>
2025-09-15 01:56:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
 LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", -100))
-ADMINS = list(map(int, os.environ.get("ADMINS", "").split(',')))
+
+# ADMINS variable ကို တန်ဖိုးမရှိရင် list အလွတ်တစ်ခု ပြန်ပေးပါ
+admins_str = os.environ.get("ADMINS", "")
+ADMINS = list(map(int, admins_str.split(','))) if admins_str else []
+
 BOT_STATUS = os.environ.get("BOT_STATUS", "on").lower()
