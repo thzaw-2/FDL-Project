e294afd40d3eb33ab93a966b9409ff8a8155e347
thazaww <thazaww@gmail.com>
2025-09-15 01:12:48 +0700

    Update defend.py

--- bot/helpers/defend.py
+++ bot/helpers/defend.py
@@ -1,10 +1,10 @@
-# bot/helpers/defend.py
 import functools
 from aiogram.types import Message
 from pyrogram.enums import ChatMemberStatus
-from bot import SmartPyro, bot
+from bot import SmartPyro
 from bot.helpers.logger import LOGGER
-from config import LOG_CHANNEL_ID
+from bot.helpers.botutils import send_message
+from config import LOG_CHANNEL_ID, BOT_STATUS, ADMINS
 
 def SmartDefender(func):
     @functools.wraps(func)
@@ -12,11 +12,23 @@ def SmartDefender(func):
         user = message.from_user
         if not user:
             return
-        
+
+        if BOT_STATUS == "off" and user.id not in ADMINS:
+            await send_message(
+                chat_id=message.chat.id,
+                text="<b>Sorry, the bot is currently in maintenance mode.</b>",
+                parse_mode=ParseMode.HTML
+            )
+            return
+
         try:
             bot_member = await SmartPyro.get_chat_member(LOG_CHANNEL_ID, "me")
             if bot_member.status not in [ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER]:
-                await message.reply("The bot needs to be an admin in the log channel to function.")
+                await send_message(
+                    chat_id=message.chat.id,
+                    text="<b>The bot needs to be an admin in the log channel to function.</b>",
+                    parse_mode=ParseMode.HTML
+                )
                 return
 
             await func(message, *args, **kwargs)
